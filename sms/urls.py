5fbe034ac09dbfffd53bd4582de587445f2c389c
Karan Mahato <44karanmahato@gmail.com>
2023-04-12 17:00:38 +0545

    finish student adding

--- sms/urls.py
+++ sms/urls.py
@@ -20,14 +20,18 @@ from sms import settings
 from sms_app import views, HodViews
 
 urlpatterns = [
-    path('demo/', views.showDemoPage),
-    path('admin/', admin.site.urls),
-    path('', views.ShowLoginPage),
-    path('doLogin', views.doLogin),
-    path('get_user_details', views.GetUserDetails),
-    path('logout_user', views.logout_user),
-    path('admin_home', HodViews.admin_home),
-    path('add_staff', HodViews.add_staff),
-    path('add_staff_save', HodViews.add_staff_save),
+                  path('demo/', views.showDemoPage),
+                  path('admin/', admin.site.urls),
+                  path('', views.ShowLoginPage),
+                  path('doLogin', views.doLogin),
+                  path('get_user_details', views.GetUserDetails),
+                  path('logout_user', views.logout_user),
+                  path('admin_home', HodViews.admin_home),
+                  path('add_staff', HodViews.add_staff),
+                  path('add_staff_save', HodViews.add_staff_save),
+                  path('add_course', HodViews.add_course),
+                  path('add_course_save', HodViews.add_course_save),
+                  path('add_student', HodViews.add_student),
+                  path('add_student_save', HodViews.add_student_save),
 
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+              ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
